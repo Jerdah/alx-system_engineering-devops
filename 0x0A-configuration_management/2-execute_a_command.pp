d89f44a7fac198c3c164f25046f066d72b470ae5
Jerdah <idjerop@gmail.com>
2024-07-01 21:56:18 +0300

    updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # File: 2-execute_a_command.pp
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin',
-  refreshonly => true,
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin:/bin',
 }
